dfa069ee6355419f3f5959ae5faa90fbe6922805
Creator125 <85368990+Creator125@users.noreply.github.com>
2023-08-16 09:55:39 -0500

    To-Do-web/: Obteniendo el frontend del proyecto

---
+++ To-Do-web/script/script.js
@@ -0,0 +1,36 @@
+/*TIPS: *No olvides utilizar el almacenamiento local (localStorage)
+ para que las tareas queden guardadas en caso
+ de que la aplicación se cierre.*/
+
+// Codigo DOM #1
+
+// Codigo DOM #2
+
+// Codigo DOM #3
+
+// Codigo DOM #4
+
+// Codigo DOM #5
+
+// Codigo DOM #6
+
+//El sistema debe permitir EDITAR o MODIFICAR una tarea.
+
+//El sistema debe permitir ELIMINAR una tarea.
+
+//El sistema debe permitir AGREGAR una o varias tareas tarea.
+
+//El sistema deber permitir MARCAR una tarea como completada
+
+//El sistema debe permitir dar diferentes PRIORIDADES a las tareas
+//EJEMPLO:
+
+//Sacar la basura - Prioridad: media
+
+//El sistema debe permitir visualizar tareas por CATEGORÍAS o ETIQUETAS
+//EJEMPLO:
+
+/*Categorías disponibles: PENDIENTE, COMPLETADO o TODASE.T.C */
+
+//Recordar llamar las funciones displayItems() y displayFooter() para mostrar
+//las tareas en pantalla
