d2285f512f4359d4fabae7a4bd4857a984b4e6ae
Creator125 <85368990+Creator125@users.noreply.github.com>
2023-09-07 14:13:55 -0500

    Terminado ejercicios sobre ciclos

--- practica_ciclos_bucles/Ejercicio8-Hijos.js
+++ practica_ciclos_bucles/Ejercicio8-Hijos.js
@@ -4,3 +4,27 @@ tienen hijos y cuántos no tienen, además se desea conocer el promedio del núm
 de hijos
 */
 
+let usuarios = parseInt(prompt("Ingrese la cantidad de usuarios: "));
+let cantHijos;
+let contadorConHijos = 0;
+let contadorSinHijos = 0;
+
+let i = 0;
+
+while (i <= usuarios) {
+    cantHijos = parseInt(prompt("Ingrese la cantidad de hijos: "));
+
+    if (cantHijos != 0) {
+        contadorConHijos++;
+    }else{
+        contadorSinHijos++;
+    }
+
+    i++;
+}
+
+let promedioConHijos = contadorConHijos / usuarios;
+let promedioSinHijos = contadorSinHijos / usuarios;
+
+alert(`Personas con hijos: ${contadorConHijos} \nPromedio: ${promedioConHijos}\n
+    \nPersonas sin hijos: ${contadorSinHijos} \nPromedio: ${promedioSinHijos}`);
