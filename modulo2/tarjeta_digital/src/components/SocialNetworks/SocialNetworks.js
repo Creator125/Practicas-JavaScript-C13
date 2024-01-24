3666186cff0f5950142bb071c02531eaf21b4ffc
Creator125 <85368990+Creator125@users.noreply.github.com>
2024-01-24 12:05:02 -0500

    Finished the missing styles

--- modulo2/tarjeta_digital/src/components/SocialNetworks/SocialNetworks.js
+++ modulo2/tarjeta_digital/src/components/SocialNetworks/SocialNetworks.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import "./SocialNetworks.css"
 
 const SocialNetworks = (props) => {
-  return <button> {props.icon} {props.name}</button>
+  return(
+    <div className='container'>
+      <button> 
+        <div className='icon'>{props.icon}</div> 
+        <div className='name'>{props.name}</div>
+      </button> 
+    </div>
+    );
 }
 
 export default SocialNetworks;
